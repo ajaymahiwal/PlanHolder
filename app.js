06db6ef907381a1b07a47caefeaf7f474d6f3760
ajaymahiwal <ajaymahiwal5@gmail.com>
2024-01-28 17:42:21 +0530

    PlanHolder first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+require('dotenv').config()
+
+const express = require("express");
+const app = express();
+const path = require("path");
+
+//Project Setup settings
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+// Universal Middlewares
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}))
+
+app.get("/",(req,res)=>{
+    res.render("./main/home.ejs");
+})
+
+
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+});
